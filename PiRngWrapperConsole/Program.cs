71799438ce4ee37fb68caca5c720c65a1e3447f9
Adam White <arakaska@gmail.com>
2016-07-05 06:51:34 -0600

    Initial code check-in

---
+++ PiRngWrapperConsole/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using PiRngWrapperLibrary;
+
+namespace PiRngWrapper
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			byte[] randomBytes = HwrngDeviceWrapper.RandomBytes(1);
+
+			Console.WriteLine("Length: {0}", randomBytes.Length);
+			Console.WriteLine("{0:X}-{1:X}-{2:X}-{3:X}", randomBytes[0], randomBytes[1], randomBytes[2], randomBytes[3]);
+			Console.WriteLine();
+			Console.WriteLine("Hit any key to continue...");
+
+			Console.ReadLine();
+		}
+	}
+}
---
+++ PiRngWrapperLibrary/PiRngWrapperLibrary.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+using System.Diagnostics;
+
+namespace PiRngWrapperLibrary
+{
+	public static class HwrngDeviceWrapper
+	{
+		internal static string OutputDirectory = "/tmp/hwrng";
+
+		internal static string GetTempFilename()
+		{
+			return string.Format("hwrng-output-{0}.bin", DateTime.Now.ToFileTimeUtc());
+		}
+
+		public static byte[] RandomBytes(uint sizeKilobytes)
+		{
+			string rngoutFilename = string.Format("{0}/{1}", OutputDirectory, GetTempFilename());
+
+			HwrngDeviceWrapper.RunShellCommand("dd", string.Format("if=/dev/hwrng of={0} bs=1024 count={1}", rngoutFilename, sizeKilobytes));
+
+			byte[] result = File.ReadAllBytes(rngoutFilename);
+
+			HwrngDeviceWrapper.RunShellCommand("rm", rngoutFilename);
+
+			return result;
+		}
+
+		internal static void RunShellCommand(string command, string arguments)
+		{
+			ProcessStartInfo procInfo = new ProcessStartInfo();
+			procInfo.FileName = command;
+			procInfo.Arguments = arguments;// string.Format("if=/dev/hwrng of={0} bs=1024 count={1}", rngoutFilename, sizeKilobytes);
+			procInfo.UseShellExecute = false;
+			procInfo.CreateNoWindow = true;
+			procInfo.RedirectStandardOutput = true;
+			procInfo.RedirectStandardError = true;
+
+			Process proc = Process.Start(procInfo);
+			proc.WaitForExit();
+			proc.Dispose();
+
+			proc = null;
+			procInfo = null;
+		}
+	}
+}
